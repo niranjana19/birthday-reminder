c55b2d52d51efc70c87e8e68099b828841884594
niranjana <niranju19@gmail.com>
2022-10-07 11:11:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Data from './Data';
+import List from './List';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h2>Birthday Reminder setup</h2>
 }
 
 export default App;
