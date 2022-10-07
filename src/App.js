c00aaa74cc6c1ae9cb8873141f61200359c61d77
niranjana <niranju19@gmail.com>
2022-10-07 12:36:58 +0530

    styling and button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import Data from './Data';
 import List from './List';
 
 function App() {
-  return <h2>Birthday Reminder setup</h2>
+  const [people,setPeople] = useState(data)
+  return (
+  <main>
+<section className = "container">
+<h3>0 birthdays today</h3>
+<List />
+<button onClick = {()=>console.log('You clicked me')} > Clear All</button>
+
+
+</section>
+
+
+  </main>
+  );
 }
 
 export default App;
+
