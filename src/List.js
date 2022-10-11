25c848a20968edd958c3c916951499a060c65f9c
niranjana <niranju19@gmail.com>
2022-10-11 15:00:27 +0530

    initial commit

--- src/List.js
+++ src/List.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
     return(
-        <div></div>
-    )
+        <>
 
-    }
+      {people.map((person)=> {
+        const {id,name,age,image}=person;
+        return (
+            <article key = {id} className = 'person'>
+                <img src = {image} alt = {name} />
+                <div>
+                    <h4>{name}</h4>
+                    <p>{age}years</p>
+                </div>
+            </article>
+        );
+      })}
+        </>
+    );
 
-export default List
+    };
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export default [
+    {
+        id:1,
+        name:'Catherine Hegde',
+        age:23,
+        image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+
+
+    },
+    {
+        id:2,
+        name:'Avantika R Teli',
+        age:'24',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+{
+    id:3,
+    name:'Allen Sheroz',
+    age:36,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+
+},
+{
+    id:4,
+    name:'Vishwa Raj',
+    age:34,
+    image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+
+},
+{
+    id:5,
+    name:'Hannah Bhatia',
+    age:27,
+    image:'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+
+},
+
+
+
+];
