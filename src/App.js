108a84846d432b01947b1d5bacde2f9d4fd6d626
niranjana <niranju19@gmail.com>
2022-10-11 14:52:01 +0530

    Updating data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Data from './Data';
+import React, {useState} from 'react'
+
+import data from './data';
 import List from './List';
 
 function App() {
@@ -7,9 +8,10 @@ function App() {
   return (
   <main>
 <section className = "container">
-<h3>0 birthdays today</h3>
-<List />
-<button onClick = {()=>console.log('You clicked me')} > Clear All</button>
+<h3>{people.length} Birthdays today</h3>
+<List people = {people} />
+
+<button onClick = {()=>setPeople([])}> Clear All</button>
 
 
 </section>
